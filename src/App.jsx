d47d6f2d00d5d011f2306fff3193da27f74bb257
Sharone Nixon <shanix@live.de>
2022-06-09 13:14:51 +0200

    added pagination btns

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Dashboard from "./components/Dashboard";
 import Activity from "./components/Activity";
 import Rankings from "./components/Rankings";
 
+// Add different styles, to choose. (Dark mode, paralex effect mode, 
+// visible borders mode, follow mouse movement mode (three.js ?), 
+// maybe something with haptic feedback, scrolling sideways?, 
+//  )
+
 function App() {
   return (
     <div id="appComponent">
--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -10,26 +10,33 @@ export default function Activity() {
     const [activityData, setActivityData] = useState(null);
     const [nextCursor, setNextCursor] = useState(null);
     const [previousCursor, setPreviousCursor] = useState(null);
+    const [runEffect, setRunEffect] = useState(false);
 
-    const limit = 200;
+    const limit = 20;
     const eventType = "successful";
     const occuredBefore = "1654330968" // Unix timestamp 04.06.22 10:22 Uhr
     const occuredAfter = "1654330968" // Unix timestamp 04.06.22 10:22 Uhr
 
     useEffect(() => {
-
-        const getEventsData = async () => {
-            // await axios.get(`https://testnets-api.opensea.io/api/v1/events?event_type=${eventType}&only_opensea=false&offset=${offset}&limit=${limit}&occurred_before=${occuredBefore}&occurred_after=${occuredAfter}`)
-            await axios.get(`https://testnets-api.opensea.io/api/v1/events?event_type=${eventType}&only_opensea=false&limit=${limit}`)
-                .then(res => {
-                    setActivityData(res?.data?.asset_events)
-                    // console.log(res.data.asset_events)
-                })
-                .catch(err => console.log("ERRORR", err))
-        }
         getEventsData();
+        setRunEffect(runEffect)
+    }, [runEffect])
 
-    }, [])
+    const getEventsData = async (cursor = false) => {
+        // await axios.get(`https://testnets-api.opensea.io/api/v1/events?event_type=${eventType}&only_opensea=false&offset=${offset}&limit=${limit}&occurred_before=${occuredBefore}&occurred_after=${occuredAfter}`)
+        await axios.get(
+            `https://testnets-api.opensea.io/api/v1/events?event_type=${eventType}&only_opensea=false&limit=${limit}${cursor ? `&cursor=${cursor}` : ""}`
+        )
+            .then(res => {
+                setActivityData(res?.data?.asset_events)
+                setPreviousCursor(res?.data?.previous)
+                setNextCursor(res?.data?.next)
+                // console.log(res.data)
+                console.log("effect running")
+            })
+            .catch(err => console.log("ERRORR", err))
+    }
+    
 
 
     return (
@@ -74,6 +81,10 @@ export default function Activity() {
                         </div>
                     ))
                 }
+            </div>
+            <div id="pageBtns">
+                <button onClick={()=> getEventsData(previousCursor)}>prev</button>
+                <button onClick={()=> getEventsData(nextCursor)}>next</button>
             </div>
 
         </div>
