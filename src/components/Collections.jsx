9e09b81e694b6fe1b3a0e9e6edd4dc4ba4f2e0ab
Sharone Nixon <shanix@live.de>
2022-06-21 20:59:19 +0200

    adding collection data API call (Collection)

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -8,15 +8,22 @@ export default function Collections() {
     const [collectionsData, setCollectionsData] = useState(null);
 
     useEffect(() => {
-        
+        getCollectionData()
     }, [])
-
+    
+    const getCollectionData = async () => {
+        await axios.get(`https://testnets-api.opensea.io/api/v1/collections?offset=0&limit=300`)
+        // .then(res=>console.log(res.data.collections))
+        .then(res=>console.log(res.data.collections[2]))
+        // .then(res=>console.log(res.data.collections[2].primary_asset_contracts[0]))
+        .catch(err=> console.log(err))
+    }
 
     return (
         <div id="rankingsComponent">
-            <h1 style={{color: "#aaa"}}>Collections on the way...</h1>
+            <h1 style={{ color: "#aaa" }}>Collections on the way...</h1>
             <div>
-                
+
             </div>
 
 
