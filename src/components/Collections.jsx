b2eb3f40c0a6c4a43ff9bcc9badb6cec42c48512
Sharone Nixon <shanix@live.de>
2022-06-21 21:08:13 +0200

    adding collection data API call (Collection)

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -6,15 +6,17 @@ import axios from "axios";
 export default function Collections() {
 
     const [collectionsData, setCollectionsData] = useState(null);
+    const [offset, setOffset] = useState(600)
+    const [limit, setLimit] = useState(300)
 
     useEffect(() => {
         getCollectionData()
     }, [])
     
     const getCollectionData = async () => {
-        await axios.get(`https://testnets-api.opensea.io/api/v1/collections?offset=0&limit=300`)
+        await axios.get(`https://testnets-api.opensea.io/api/v1/collections?offset=${offset}&limit=${limit}`)
         // .then(res=>console.log(res.data.collections))
-        .then(res=>console.log(res.data.collections[2]))
+        .then(res=>console.log(res.data.collections[0]))
         // .then(res=>console.log(res.data.collections[2].primary_asset_contracts[0]))
         .catch(err=> console.log(err))
     }
