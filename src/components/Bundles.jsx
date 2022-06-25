ec7d279969de92cc2a92e7e2989b82c8f8b6e01b
Sharone Nixon <shanix@live.de>
2022-06-25 22:24:40 +0200

    using new API for bundle component, adjusting code

--- src/components/Bundles.jsx
+++ src/components/Bundles.jsx
@@ -12,18 +12,37 @@ export default function Bundles() {
 
     const [bundlesData, setBundlesData] = useState(null);
     const [offset, setOffset] = useState(0);
-    const [limit, setLimit] = useState(20);
-    const [runEffect, setRunEffect] = useToggleState(true);
+    const [limit, setLimit] = useState(40);
+    const [runEffect, toggleRunEffect] = useToggleState(true);
 
     useEffect(() => {
-        getBundlesDate();
+        // getBundlesData();
+        getOdersData()
     }, [runEffect]);
 
-    const getBundlesDate = async () => {
-        axios.get(`https://testnets-api.opensea.io/api/v1/bundles?limit=${limit}&offset=${offset}`)
+    const getBundlesData = async () => {
+        await axios.get(`https://testnets-api.opensea.io/api/v1/bundles?limit=${limit}&offset=${offset}`)
             .then(res => setBundlesData(res.data.bundles))
             .catch(err => console.log(err));
+        topActivity?.current?.scrollIntoView();
     };
+    const getOdersData = async () => {
+        await axios.get(`https://testnets-api.opensea.io/wyvern/v1/orders?bundled=true&include_bundled=false&side=1&limit=50&offset=0&order_by=created_date&order_direction=desc`)
+            .then(res => setBundlesData(res.data.orders))
+            .catch(err => console.log(err));
+        topActivity?.current?.scrollIntoView();
+    };
+
+
+    const handlePrevClick = () => {
+        if (offset === 0) return
+        if (offset > 0) setOffset(offset - limit);
+        toggleRunEffect();
+    }
+    const handleNextClick = () => {
+        setOffset(offset + limit);
+        toggleRunEffect();
+    }
 
     return (
         <div id="bundleComponent">
@@ -31,11 +50,11 @@ export default function Bundles() {
             <main id="bundlesContainer">
                 <div ref={topActivity}></div>
                 {
-                    bundlesData && bundlesData.map((bundle, i) => (
-                        <div className="bundle card" key={i}>
+                    bundlesData && bundlesData.map(bundle => (
+                        <div className="bundle card" key={bundle.id}>
                             <div id="imagesContainer">
                                 {
-                                    bundle?.assets?.map((asset, i) => (
+                                    bundle?.asset_bundle?.assets?.map((asset, i) => (
                                         <div key={asset.id}>
                                             {
                                                 i <= 1 &&
@@ -51,30 +70,30 @@ export default function Bundles() {
                             </div>
                             <div id="infoSection">
                                 {/* _______ end of the sale ________________________________________ */}
-                                <div className="infoRows">
+                                <div>
                                     {
-                                        bundle?.sell_orders ?
-                                        <div>
-                                            {/* _______ timeStamp ___________________________ */}
-                                            <div className="saleEndDate">
-                                                {`Sale ends on ${new Date(bundle.sell_orders[0].closing_date).toLocaleString().slice(0,16)} UTC`}
+                                        bundle?.closing_date ?
+                                            <div>
+                                                {/* _______ timeStamp ___________________________ */}
+                                                <div className="saleEndDate">
+                                                    {`Sale ends on ${new Date(bundle?.closing_date).toLocaleString().slice(0, 16)} UTC`}
+                                                </div>
                                             </div>
-                                        </div>
-                                        :
-                                        <div className="saleEndDate">
-                                            Sale has been cancelled
-                                        </div>
+                                            :
+                                            <div className="saleEndDate">
+                                                Sale has ended
+                                            </div>
                                     }
                                 </div>
                                 {/* _______ bundle name ________________________________________ */}
-                                <div className="infoRows">
+                                <div className="infoRows bundleName">
                                     {
-                                        bundle?.name.length > 30 ? `${bundle.name.slice(0, 30)}...` : bundle?.name
+                                        // bundle?.name.length > 30 ? `${bundle.name.slice(0, 30)}...` : bundle?.name
                                     }
                                 </div>
 
                                 <div>
-                                    <a href={bundle.permalink} target="_blank" rel="noreferrer">
+                                    <a href={bundle?.asset_bundle?.permalink} target="_blank" rel="noreferrer">
                                         <img src={OpenseaImage} alt="Opensea Link" className="openseaLink" />
                                     </a>
                                 </div>
@@ -91,16 +110,15 @@ export default function Bundles() {
             <div id="pageBtns">
                 {/* __________ Previous button ______________________________________ */}
                 <button
-
-
-                    className="pageButton "
+                    disabled={offset === 0 ? true : false}
+                    onClick={handlePrevClick}
+                    className="pageButton"
                 >
                     prev
                 </button>
                 {/* __________ Next button ______________________________________ */}
                 <button
-
-
+                    onClick={handleNextClick}
                     className="pageButton"
                 >
                     next
