8e4d5cb340b8235e4c9eb7a387227d55aa7e7ef4
Sharone Nixon <shanix@live.de>
2022-06-28 11:41:22 +0200

    add connect to MetaMask + css code fixes

--- src/components/MyWallet.jsx
+++ src/components/MyWallet.jsx
@@ -1,35 +1,55 @@
 
-import { useState } from "react"
+import "./styles/myWallet.css"
+import { useEffect, useState } from "react"
 
 export default function MyWallet() {
 
   const [accounts, setAccounts] = useState(null)
+  const [connecting, setConnecting] = useState(false)
 
   const connectMetaMask = async () => {
+    setConnecting(true)
 
     // ____ if MetaMask isn't installed on the current browser, _______
     // ____  alert the user to install it and end this function _______
-    if (typeof window.ethereum === "undefined") return alert("please install MetaMask")
+    if (typeof window.ethereum === "undefined") {
+      alert(`Please install the MetaMask browser extention`)
+      return setConnecting(false)
+    }
 
-    // ____ to check if MetaMask is installed to the current browser ____________
+    // ____ to request account/wallet from MM and save it in accounts state ____________
     if (typeof window.ethereum !== 'undefined') {
       console.log('MetaMask is ready!');
       await window.ethereum.request({ method: 'eth_requestAccounts' })
         .then(res => setAccounts(res))
         .catch(err => console.log(err))
-
-
+      return setConnecting(false)
     }
   }
 
-
+  // useEffect(()=>{
+  //   connectMetaMask()
+  // },[])
 
 
 
   return (
-    <div>
-      <h1 style={{ color: "#aaa" }}>Connect your wallet in the near future</h1>
-      <button onClick={connectMetaMask}> Connect MetaMask</button>
+    <div className="myWalletComponent">
+      <div className="walletContainer">
+
+        {
+          accounts ?
+            <div>Connectedere</div>
+            :
+            <div className="connectWalletSection">
+              <button onClick={connectMetaMask} disabled={connecting} className="pageButton">
+                {
+                  !connecting ? "Connect to MetaMask" : "Connecting"
+                }
+              </button>
+            </div>
+        }
+      </div>
     </div>
   )
 }
