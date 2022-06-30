345018c6e0971cac89689e2a155a55379daeadf3
Sharone Nixon <shanix@live.de>
2022-06-30 10:03:04 +0200

    added MM connection with web3-react

--- src/components/MyWallet.jsx
+++ src/components/MyWallet.jsx
@@ -1,97 +1,41 @@
 
 import "./styles/myWallet.css"
 import { useEffect, useState } from "react"
+import { injected } from "./wallet/connectors"
+import { useWeb3React } from "@web3-react/core"
 
 export default function MyWallet() {
 
-  const [accounts, setAccounts] = useState(null)
-  const [connecting, setConnecting] = useState(false)
+  const { active, account, library, connector, activate, deactivate } = useWeb3React();
 
-  const requestAccounts = async () => {
-    await window.ethereum.request({ method: 'eth_requestAccounts' })
-      .then(res => setAccounts(res))
-      .catch(err => console.log(err))
-  }
-
-  const connectMetaMask = async () => {
-    setConnecting(true)
-    // ____ if MetaMask isn't installed on the current browser, _______
-    // ____  alert the user to install it and end this function _______
-    if (typeof window.ethereum === "undefined") {
-      alert(`Please install the MetaMask browser extention`)
-      return setConnecting(false)
-    }
-
-    try {
-      await window.ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        // params: [{ chainId: '0x1' }], // __ ETH chainId ___
-        params: [{ chainId: '0x4' }], // __ Rinkeby Testnet chainId ___
-      });
-    } catch (switchError) {
-      // This error code indicates that the chain has not been added to MetaMask.
-      if (switchError.code === 4902) {
-        // ____ Add Rinkeby testnetwork to MM wallet ________
-        try {
-          await window.ethereum.request({
-            method: 'wallet_addEthereumChain',
-            params: [
-              {
-                chainId: '0x4',
-                chainName: 'Rinkeby Testnet',
-                rpcUrls: ['https://rinkeby.infura.io/v3/'],
-                nativeCurrency: {
-                  name: "Rinky Eth",
-                  symbol: "RinkebyETH", // 2-6 characters long
-                  decimals: 18,
-                },
-                blockExplorerUrls: ["https://https://rinkeby.etherscan.io.io"]
-              },
-            ],
-          });
-          // ____ Add ethereum mainnet to MM wallet ______ (COLLAPSED)!!!!!!!
-          // await window.ethereum.request({
-          //   method: 'wallet_addEthereumChain',
-          //   params: [
-          //     {
-          //       chainId: '0x1',
-          //       chainName: 'Ethereum Mainnet',
-          //       rpcUrls: ['https://mainnet.infura.io/v3/'],
-          //       nativeCurrency: {
-          //         name: "Eth",
-          //         symbol: "ETH", // 2-6 characters long
-          //         decimals: 18,
-          //       },
-          //       blockExplorerUrls: ["https://etherscan.io"]
-          //     },
-          //   ],
-          // });
-        } catch (addError) {
-          console.log(addError)
-        }
-      }
-      // handle other "switch" errors
-    }
-
-    // ____ to request account/wallet from MM and save it in accounts state ____________
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccounts();
-      return setConnecting(false)
-    }
-  }
 
   const checkIsConnected = async () => {
     window.ethereum.request({ method: 'eth_accounts' })
-      .then(res=> res[0] && requestAccounts())
+      .then(res => res[0] && activate(injected))
       .catch(err => console.log(err))
   }
 
   useEffect(() => {
-    checkIsConnected();
+    if (typeof window.ethereum !== 'undefined') checkIsConnected();
     console.log("effect runninfrs")
   }, [])
 
-  // console.log(window.ethereum.isConnected())
+  // _____ connect website to wallet ______________
+  const connect = async () => {
+    try {
+      await activate(injected)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  // _____ disconnect website to wallet ____________
+  const disconnect = async () => {
+    try {
+      await deactivate(injected)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   return (
@@ -99,16 +43,14 @@ export default function MyWallet() {
       <div className="walletContainer">
 
         {
-          accounts ?
+          active ?
             <div className="walletSection">
-              is Connected
+              {account} is Connected
             </div>
             :
             <div className="connectWalletSection">
-              <button onClick={connectMetaMask} disabled={connecting} className="pageButton">
-                {
-                  !connecting ? "Connect to MetaMask" : "Connecting"
-                }
+              <button onClick={connect}  className="pageButton">
+                 Connect to MetaMask
               </button>
             </div>
         }
