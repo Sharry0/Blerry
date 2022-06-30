4c2be750b7669d604ccb262726ef8ccbd38f9939
Sharone Nixon <shanix@live.de>
2022-06-30 21:38:24 +0200

    adding token balance

--- src/components/MyWallet/TokenBalance.jsx
+++ src/components/MyWallet/TokenBalance.jsx
@@ -1,40 +1,63 @@
 
+import { useEffect } from "react"
+import { useWeb3React } from "@web3-react/core"
+import {formatUnits } from "@ethersproject/units"
+import {isAddress} from "@ethersproject/address"
+import {Contract} from "@ethersproject/contracts"
+import useSWR from "swr"
+
+
+
+const fetcher = (library, abi) => (...args) => {
+  const [arg1, arg2, ...params] = args
+  // it's a contract
+  if (isAddress(arg1)) {
+    const address = arg1
+    const method = arg2
+    const contract = new Contract(address, abi, library.getSigner())
+    return contract[method](...params)
+  }
+  // it's a eth call
+  const method = arg1
+  return library[method](arg2, ...params)
+}
+
 
 
 export const TokenBalance = ({ symbol, address, decimals }) => {
-    const { account, library } = useWeb3React()
-    const { data: balance, mutate } = useSWR([address, 'balanceOf', account], {
-      fetcher: fetcher(library, ERC20ABI),
+  const { account, library } = useWeb3React()
+  const { data: balance, mutate } = useSWR([address, 'balanceOf', account], {
+    fetcher: fetcher(library, ERC20ABI),
+  })
+
+  useEffect(() => {
+    // listen for changes on an Ethereum address
+    console.log(`listening for Transfer...`)
+    const contract = new Contract(address, ERC20ABI, library.getSigner())
+    const fromMe = contract.filters.Transfer(account, null)
+    library.on(fromMe, (from, to, amount, event) => {
+      console.log('Transfer|sent', { from, to, amount, event })
+      mutate(undefined, true)
     })
-  
-    useEffect(() => {
-      // listen for changes on an Ethereum address
-      console.log(`listening for Transfer...`)
-      const contract = new Contract(address, ERC20ABI, library.getSigner())
-      const fromMe = contract.filters.Transfer(account, null)
-      library.on(fromMe, (from, to, amount, event) => {
-        console.log('Transfer|sent', { from, to, amount, event })
-        mutate(undefined, true)
-      })
-      const toMe = contract.filters.Transfer(null, account)
-      library.on(toMe, (from, to, amount, event) => {
-        console.log('Transfer|received', { from, to, amount, event })
-        mutate(undefined, true)
-      })
-      // remove listener when the component is unmounted
-      return () => {
-        library.removeAllListeners(toMe)
-        library.removeAllListeners(fromMe)
-      }
-      // trigger the effect only on component mount
-    }, [])
-  
-    if (!balance) {
-      return <div>...</div>
+    const toMe = contract.filters.Transfer(null, account)
+    library.on(toMe, (from, to, amount, event) => {
+      console.log('Transfer|received', { from, to, amount, event })
+      mutate(undefined, true)
+    })
+    // remove listener when the component is unmounted
+    return () => {
+      library.removeAllListeners(toMe)
+      library.removeAllListeners(fromMe)
     }
-    return (
-      <div>
-        {parseFloat(formatUnits(balance, decimals)).toPrecision(4)} {symbol}
-      </div>
-    )
+    // trigger the effect only on component mount
+  }, [])
+
+  if (!balance) {
+    return <div>...</div>
   }
+  return (
+    <div>
+      {parseFloat(formatUnits(balance, decimals)).toPrecision(4)} {symbol}
+    </div>
+  )
+}
---
+++ src/components/MyWallet/wallet/tokenBalance.js
@@ -0,0 +1,34 @@
+
+
+export const Networks = {
+    MainNet: 1,
+    Rinkeby: 4,
+    Ropsten: 3,
+    Kovan: 42,
+}
+
+// export interface IERC20 {
+//     symbol: string
+//     address: string
+//     decimals: number
+//     name: string
+// }
+
+// export const TOKENS_BY_NETWORK: {
+//     [key]: IERC20[]
+// } = {
+//     [Networks.Rinkeby]: [
+//         {
+//             address: "0x5592EC0cfb4dbc12D3aB100b257153436a1f0FEa",
+//             symbol: "DAI",
+//             name: "Dai",
+//             decimals: 18,
+//         },
+//         {
+//             address: "0xF9bA5210F91D0474bd1e1DcDAeC4C58E359AaD85",
+//             symbol: "MKR",
+//             name: "Maker",
+//             decimals: 18,
+//         },
+//     ],
+// }
