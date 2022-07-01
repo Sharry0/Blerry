a509511ec9f350dc123edee8e94820f6026ed26e
Sharone Nixon <shanix@live.de>
2022-07-01 10:04:29 +0200

    :coffin:

--- src/components/MyWallet/TokenList.jsx
+++ src/components/MyWallet/TokenList.jsx
@@ -5,8 +5,6 @@ import { TOKENS_BY_NETWORK } from "./wallet/tokenBalance"
 
 export default function TokenList({ chainId }) {
 
-  console.log(TOKENS_BY_NETWORK[chainId])
-
   return (
     <>
       {TOKENS_BY_NETWORK[chainId]?.map((token) => (
