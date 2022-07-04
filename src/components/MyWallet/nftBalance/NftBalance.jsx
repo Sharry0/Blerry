f419e20c001682fefd0c10588341db06e4e92bde
Sharone Nixon <shanix@live.de>
2022-07-04 13:18:16 +0200

    :bug: updated contract address to the RIGHT ON ams

--- src/components/MyWallet/nftBalance/NftBalance.jsx
+++ src/components/MyWallet/nftBalance/NftBalance.jsx
@@ -12,50 +12,55 @@ import useSWR from 'swr';
 
 export default function NftBalance({ tokenId }) {
 
-    const addressContract = "0x32B80E878F0658EcA9b0d9D944e60e0c7B3289D0";
+    const addressContract = "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b";
     // console.log(tokenId)
 
     const { account, active, library } = useWeb3React()
     const [itemInfo, setItemInfo] = useState()
 
     // const { data: nftURI } = useSWR([addressContract, 'tokenURI', 1449261])
-    const { data: nftURI, mutate } = useSWR([addressContract, 'tokenURI', "1449261"], {
+    const { data, error } = useSWR([addressContract, 'tokenURI', 1449261], {
         fetcher: fetcher(library, MFAbi)
     });
     const contract = new Contract(addressContract, MFAbi, library)
 
+    console.log(data)
+    
 
     // console.log(await contract.tokenURI(tokenId))
 
     useEffect(() => {
-        if (!nftURI) return
-        console.log(nftURI)
+        // if (!nftURI) return
+        if (!data) return
         const contract = new Contract(addressContract, MFAbi, library)
         const fromMe = contract.filters.Transfer(account, null)
 
         // console.log(contract)
-        library.on(fromMe, (from, to, amount, event) => {
-            console.log('Transfer|sent', { from, to, amount, event })
-            mutate(undefined, true)
-        })
+        // library.on(fromMe, (from, to, amount, event) => {
+        //     console.log('Transfer|sent', { from, to, amount, event })
+        //     mutate(undefined, true)
+        // })
 
-        const toMe = contract.filters.Transfer(null, account)
-        library.on(toMe, (from, to, amount, event) => {
-            console.log('Transfer|received', { from, to, amount, event })
-            mutate(undefined, true)
-        })
+        // const toMe = contract.filters.Transfer(null, account)
+        // library.on(toMe, (from, to, amount, event) => {
+        //     console.log('Transfer|received', { from, to, amount, event })
+        //     mutate(undefined, true)
+        // })
 
         // remove listener when the component is unmounted
         return () => {
-            library.removeAllListeners(toMe)
-            library.removeAllListeners(fromMe)
+            // library.removeAllListeners(toMe)
+            // library.removeAllListeners(fromMe)
         }
         // some()
     }, [])
 
 
     return (
-        <div>NftBalance</div>
+        <div>
+            
+            {error && console.log(error)}
+        </div>
     )
 }
 
