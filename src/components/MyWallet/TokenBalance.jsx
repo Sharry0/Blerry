8f993259cc04f143e71330660af15e68c5fc285a
Sharone Nixon <shanix@live.de>
2022-07-05 13:06:03 +0200

    code clean up

--- src/components/MyWallet/TokenBalance.jsx
+++ src/components/MyWallet/TokenBalance.jsx
@@ -1,44 +1,44 @@
 
-import { useEffect } from "react"
-import { useWeb3React } from "@web3-react/core"
-import {formatUnits } from "@ethersproject/units"
-import {Contract} from "@ethersproject/contracts"
-import { fetcher } from "./utils"
-import ERC20ABI from "./abi/erc20Abi.json"
-import useSWR from "swr"
+import { useEffect } from "react";
+import { useWeb3React } from "@web3-react/core";
+import {formatUnits } from "@ethersproject/units";
+import {Contract} from "@ethersproject/contracts";
+import { fetcher } from "./utils";
+import ERC20ABI from "./abi/erc20Abi.json";
+import useSWR from "swr";
 
 
 export default function TokenBalance  ({ symbol, address, decimals }) {
   const { account, library } = useWeb3React()
   const { data: balance, mutate } = useSWR([address, 'balanceOf', account], {
     fetcher: fetcher(library, ERC20ABI)
-  })
+  });
 
   useEffect(() => {
     // listen for changes on an Ethereum address
     // console.log(`listening for Transfer...`)
-    const contract = new Contract(address, ERC20ABI, library.getSigner())
-    const fromMe = contract.filters.Transfer(account, null)
+    const contract = new Contract(address, ERC20ABI, library.getSigner());
+    const fromMe = contract.filters.Transfer(account, null);
 
     // console.log(contract)
     library.on(fromMe, (from, to, amount, event) => {
-      console.log('Transfer|sent', { from, to, amount, event })
-      mutate(undefined, true)
-    })
+      console.log('Transfer|sent', { from, to, amount, event });
+      mutate(undefined, true);
+    });
 
-    const toMe = contract.filters.Transfer(null, account)
+    const toMe = contract.filters.Transfer(null, account);
     library.on(toMe, (from, to, amount, event) => {
-      console.log('Transfer|received', { from, to, amount, event })
-      mutate(undefined, true)
-    })
+      console.log('Transfer|received', { from, to, amount, event });
+      mutate(undefined, true);
+    });
 
     // remove listener when the component is unmounted
     return () => {
-      library.removeAllListeners(toMe)
-      library.removeAllListeners(fromMe)
-    }
+      library.removeAllListeners(toMe);
+      library.removeAllListeners(fromMe);
+    };
     // trigger the effect only on component mount
-  }, [])
+  }, []);
 
   if (!balance) {
     return <div>...</div>
--- src/components/MyWallet/nftBalance/NftBalance.jsx
+++ src/components/MyWallet/nftBalance/NftBalance.jsx
@@ -1,6 +1,6 @@
 
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { useWeb3React } from '@web3-react/core';
 import MFAbi from "../abi/MFAbi.json"
 import { fetcher } from '../utils';
@@ -13,23 +13,24 @@ export default function NftBalance({ tokenId }) {
     const addressContract = "0xf5de760f2e916647fd766B4AD9E85ff943cE3A2b";
 
 
-    const { account, active, library } = useWeb3React()
-    const [itemInfo, setItemInfo] = useState(null)
+    const { account, active, library } = useWeb3React();
+    const [itemInfo, setItemInfo] = useState(null);
 
     const { data: nftURI } = useSWR([addressContract, 'tokenURI', 1449261], {
-        fetcher: fetcher(library, MFAbi)
+        fetcher: fetcher(library, MFAbi);
     });
 
     // console.log(nftURI)
 
     useEffect(() => {
-        if (!nftURI) return
+        if (!nftURI) return;
         
         
 
-        const data = base64.decode(nftURI.slice(21))
+        const data = base64.decode(nftURI.slice(21));
 
-        console.log(data)
+        console.log(data);
+        console.log(nftURI);
         // console.log(`https://${nftURI.slice(21)}.ipfs.dweb.link`)
 
         fetch(`https://${nftURI.slice(21)}.ipfs.dweb.link`)
@@ -42,7 +43,7 @@ export default function NftBalance({ tokenId }) {
         //   "svg":svg})
 
         // some()
-    }, [nftURI])
+    }, [nftURI]);
 
 
     return (
