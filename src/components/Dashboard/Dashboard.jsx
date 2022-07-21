1946476778687febbfc3f523bea0c1cf9d5b0022
Sharone Nixon <shanix@live.de>
2022-07-21 17:45:52 +0200

    adding CryptoSlam api (top100 nft collections)

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,22 @@
 
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 
 export default function Dashboard() {
-    
+  
+  const [top, setTop] = useState();
+
+  const get = async()=>{
+    await axios.get("https://api.cryptoslam.io/v1/collections/top-100?timeRange=week")
+      .then(res=> setTop(res.data))
+      .catch(err=> console.log(err))
+  }
+
+  useEffect(()=>{
+    get();
+  },[])
+
   return (
     <div>
       <h1 style={{color: "#aaa"}}>Dashboard on the way...</h1>
