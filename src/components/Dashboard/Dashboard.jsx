cc0f77f8a02412f0e8d304dd61c80fe48ab9f0c0
Sharone Nixon <shanix@live.de>
2022-07-23 13:05:21 +0200

    added styling + axis labels to chart diagramm

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+import "./dashboard.css"
 
 import axios from "axios"
 import { useState, useEffect } from "react"
@@ -44,18 +45,56 @@ export default function Dashboard() {
           boxWidth: 0,
           boxHeight: 0
         },
+
       },
-      tooltip:{
+      tooltip: {
         backgroundColor: "rgba(51, 57, 68, 0.8)"
       },
       title: {
         display: true,
-        text: `Top ${dataShown} NFT sales of the ${timeRange}`,
+        text: `Top ${dataShown} NFT sales of the last ${timeRange}`,
       },
     },
-    parsing:{
+    parsing: {
       xAxisKey: "contractName",
       yAxisKey: "valueUSD"
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: 'Collections',
+          color: '#012a4a',
+          font: {
+            family: 'Quicksand',
+            size: 30,
+            weight: 'bold',
+            lineHeight: 1.2,
+          },
+          padding: {top: 5, left: 0, right: 0, bottom: 0}
+        },
+        
+      },
+      y: {
+        display: true,
+        title: {
+          display: true,
+          text: 'US Dollar ($)',
+          color: '#013a63',
+          font: {
+            family: 'Quicksand',
+            size: 30,
+            style: 'normal',
+            lineHeight: 1.2
+          },
+          padding: {top: 30, left: 0, right: 0, bottom: 0}
+        },
+        grid: {
+          display: true,
+          color: "#012a4a99"
+        }
+      }
     }
   };
 
@@ -66,7 +105,7 @@ export default function Dashboard() {
     datasets: [
       {
         label: 'Sales USD',
-        data: top?.filter((collection, i) =>  i< dataShown && collection),
+        data: top?.filter((collection, i) => i < dataShown && collection),
         // data: top?.filter((collection, i) =>  i<= dataShown && collection)?.map(collection=> collection.valueUSD),
         backgroundColor: ["#a9d6e5", "#89c2d9", "#61a5c2", "#468faf", "#2c7da0", "#2a6f97", "#014f86", "#01497c", "#013a63", "#012a4a", "#143642"]
       },
@@ -80,9 +119,12 @@ export default function Dashboard() {
 
   return (
     <div className="dashboardComponent">
-      <h1 style={{ color: "#aaa" }}>Dashboard on the way...</h1>
 
-      <Bar options={options} data={data} />
+      <div className="chart">
+
+        <Bar options={options} data={data} />
+      </div>
+
 
     </div>
   )
