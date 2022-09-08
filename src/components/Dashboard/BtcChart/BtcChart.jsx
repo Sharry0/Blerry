7c9085e8652e62b7f636f988526970cc87d38b12
Sharone Nixon <shanix@live.de>
2022-09-08 17:59:09 +0200

    added locale date converter to x axis

--- src/components/Dashboard/BtcChart/BtcChart.jsx
+++ src/components/Dashboard/BtcChart/BtcChart.jsx
@@ -15,12 +15,19 @@ import axios from "axios"
 
 export default function BtcChart() {
 
-    const [btcHistory, setBtcHistory] = useState();
+    const [btcHistory, setBtcHistory] = useState(false);
+    const [currency, setCurrency] = useState("usd");
+    const [days, setDays] = useState("3");
+    const months = ["Jan", "Feb", "Mar", "Apr", 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-    
+    const getBtcHistory = () => {
+        axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=${currency}&days=${days}`)
+            .then(res => setBtcHistory(res.data))
+            .catch(err => console.log(err))
+    };
 
     useEffect(() => {
-
+        getBtcHistory();
     }, []);
 
     ChartJS.register(
@@ -33,23 +40,44 @@ export default function BtcChart() {
         Legend
     );
 
-    const options ={
+    const options = {
+        plugins: {
+            title: {
+                display: true,
+                text: `Bitcoin (${currency})`
+            },
+            tooltip: {
+                interaction: false,
+                callbacks: {
+                    label: function (context) {
+                        // console.log(context)
+                        return ` ${context.formattedValue} ${currency}`;
+                    },
+                }
+            }
+        },
+    };
 
+    const unixTimeToDateTime = (unixTime) => {
+        const s = new Date(unixTime)
+        const month = s.getMonth()
+        const day = s.getDate()
+        return `${day}. ${months[month]}`
     };
 
     const data = {
-        // labels: pieData && Object?.keys(pieData?.market_cap_percentage),
+        labels: btcHistory && btcHistory.prices.map(unix => unixTimeToDateTime(unix[0])),
         datasets: [
             {
-                // data: pieData && Object.values(pieData?.market_cap_percentage),
-                backgroundColor: ["#4A719CDD", "#A5D4DFDD", "#333944DD", "#B3B6C7DD",],
+                data: btcHistory && btcHistory.prices.map(unix => unix[1]),
+                backgroundColor: "#4A719CDD",
                 borderWidth: 2,
             }
         ]
     };
 
     return (
-        <div >
+        <div className="btcHistoryComponent">
             <div className="chart">
                 <Line options={options} data={data} />
             </div>
