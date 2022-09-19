966dcf4cff2bf14c1deb5a9c135c644e8920a62d
Sharone Nixon <shanix@live.de>
2022-09-19 17:34:13 +0200

    rm unused component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 
 import "./dashboard.css";
-import Coins from "./Coins/Coins";
 import TotalMarket from "./TotalMarketPie/TotalMarket";
 import TopTenList from "./TopTen/TopTenList";
 import BtcChart from "./BtcChart/BtcChart";
