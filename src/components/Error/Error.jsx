ffa079e1f71cbf36d0b5377d03b087aad4f4f039
Sharone Nixon <shanix@live.de>
2022-10-18 18:07:48 +0200

    added error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,13 +1,16 @@
 
-import css from "./Error.module.css"
-import blerryLogo from "../../images/blerry_logo.svg"
+import css from "./Error.module.css";
+import bitenCherry from "../../images/biten_cherry.png";
 
 export default function Error() {
 
   return (
     <div className={css.errorComponent}>
-      <img className={css.errorImage} src={blerryLogo} alt="blerry" />
-      <h1>{"Oops! Looks like something went wrong"}</h1>
+      <img src={bitenCherry} alt="cherry with a bite" />
+      <div>
+        <h1>{"Ohh no! Looks like something went wrong"}</h1>
+        <h3>Please try again later ...</h3>
+      </div>
     </div>
   )
 }
